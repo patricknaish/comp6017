8dc1f407a6630ce9e292b8d5e4526743d19c7b4f
Patrick Naish <patrick@pmnaish.co.uk>
2013-12-16 21:17:39 +0000

    Added test for cascading deletes on answers

--- myapp/test.sh
+++ myapp/test.sh
@@ -58,7 +58,7 @@ tput sgr0
 curl -s http://localhost:3000/user/1 | python -mjson.tool
 
 tput setaf 2
-echo "\nTest cascading deletes"
+echo "\nTest cascading deletes on questions"
 tput setaf 3
 echo Creating question...
 curl -sX POST -d "title=Test+Question&question=This+is+a+test&author_id=1" http://localhost:3000/question > /dev/null
@@ -75,5 +75,26 @@ curl -sX DELETE http://localhost:3000/question/1 > /dev/null
 echo Listing comments...
 tput sgr0
 curl -s http://localhost:3000/question/1/comment | python -mjson.tool
+
+tput setaf 2
+echo "\nTest cascading deletes on answers"
+tput setaf 3
+echo Creating question...
+curl -sX POST -d "title=Test+Question&question=This+is+a+test&author_id=1" http://localhost:3000/question > /dev/null
+echo Creating answer...
+curl -sX POST -d "answer=This+is+a+test+answer&author_id=2" http://localhost:3000/question/1/answer > /dev/null
+echo Adding first comment...
+curl -sX POST -d "comment=This+is+a+test+comment&author_id=1" http://localhost:3000/question/1/answer/1/comment > /dev/null
+echo Adding second comment...
+curl -sX POST -d "comment=This+is+another+test+comment&author_id=1" http://localhost:3000/question/1/answer/1/comment > /dev/null
+echo Listing comments...
+tput sgr0
+curl -s http://localhost:3000/question/1/answer/1/comment | python -mjson.tool
+tput setaf 3
+echo Deleting answer...
+curl -sX DELETE http://localhost:3000/question/1/answer/1 > /dev/null
+echo Listing comments...
+tput sgr0
+curl -s http://localhost:3000/question/1/answer/1/comment | python -mjson.tool
 
 killall node
